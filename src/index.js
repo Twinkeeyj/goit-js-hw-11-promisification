67dfc626c6d72f056ecd5e633972e3a79e9d9b6a
Twinkeeyj <68866868+Twinkeeyj@users.noreply.github.com>
2020-10-27 21:07:44 +0200

    finish

---
+++ src/index.js
@@ -0,0 +1,9 @@
+
+import task_1 from './components/task_1'
+import task_2 from './components/task_2'
+import task_3 from './components/task_3'
+
+
+// task_1().delay()
+// task_2().fn()
+// task_3().fn()
